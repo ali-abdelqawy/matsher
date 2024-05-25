003f0eb08503d7709c7b032613e7e5cfac3223cb
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-25 17:35:30 +0300

    add types to process.env

---
+++ src/core/global/global.d.ts
@@ -0,0 +1,12 @@
+import { NODE_ENV } from "../env";
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: NODE_ENV;
+      PORT: string;
+    }
+  }
+}
+
+export {};
