535b37504093694df243b232a72472ac77e0d922
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-25 22:32:45 +0300

    update property requests constants

--- src/app/@properties/requests/property-requests.consts.ts
+++ src/app/@properties/requests/property-requests.consts.ts
@@ -1,3 +1,3 @@
 export const PROPERTY_TYPES = ["VILLA", "HOUSE", "LAND", "APARTMENT"] as const;
-export const PROPERTY_AREA_UNITS = ["meters", "kilo_meters"];
-export const PROPERTY_PRICE_UNITS = ["sar"];
+export const PROPERTY_AREA_UNITS = ["METERS", "KILO_METERS"];
+export const PROPERTY_PRICE_UNITS = ["SAR"];
