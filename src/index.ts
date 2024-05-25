060f7d84fe225aa11b1fad06975500e516e2527c
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-25 18:32:34 +0300

    install routing-controllers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import express from "express";
+import path from "path";
 import { Mongo } from "./core/db";
+import { createExpressServer } from "routing-controllers";
 
 Mongo.get().connect();
 
-const app = express();
-
-app.get("/", (_req, res) => {
-  res.send("Welcome to Matsher!");
+const app = createExpressServer({
+  routePrefix: "/api",
+  validation: {
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  },
+  middlewares: [],
+  controllers: [path.join(__dirname + "/**/*.controller.{js,ts}")],
 });
 
 app.listen(process.env.PORT, () => console.log("matsher api is up and running!"));
