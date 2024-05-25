4d2044f635fa4849cb4f30f21b8fcdc9830359cf
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-26 01:23:47 +0300

    extract schemas defintions

--- src/app/properties/requests/property-requests.schema.ts
+++ src/app/properties/requests/property-requests.schema.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Types } from "mongoose";
+import { Schema, model, Types, InferRawDocType } from "mongoose";
 import { PROPERTY_AREA_UNITS, PROPERTY_PRICE_UNITS, PROPERTY_TYPES } from "./property-requests.consts";
 
 export const AreaSchema = new Schema(
@@ -21,19 +21,19 @@ export const PriceSchema = new Schema(
   }
 );
 
-const PropertyRequestSchema = new Schema(
-  {
-    propertyType: { type: String, enum: PROPERTY_TYPES, required: true },
-    area: AreaSchema,
-    price: PriceSchema,
-    city: { type: String, required: true },
-    district: { type: String, required: true },
-    description: { type: String, required: true },
-    refreshedAt: { type: Date, required: false },
-  },
-  {
-    timestamps: true,
-  }
-);
+const PropertyRequestSchemaDefinition = {
+  propertyType: { type: String, enum: PROPERTY_TYPES, required: true },
+  area: AreaSchema,
+  price: PriceSchema,
+  city: { type: String, required: true },
+  district: { type: String, required: true },
+  description: { type: String, required: true },
+  refreshedAt: { type: Date, required: false },
+};
+
+const PropertyRequestSchema = new Schema(PropertyRequestSchemaDefinition, {
+  timestamps: true,
+});
 
 export const PropertyRequest = model("PropertyRequest", PropertyRequestSchema);
+export type PropertyRequestFilter = Partial<InferRawDocType<typeof PropertyRequestSchemaDefinition>>;
--- src/app/users/users.schema.ts
+++ src/app/users/users.schema.ts
@@ -1,17 +1,17 @@
-import { Schema, model } from "mongoose";
+import { InferRawDocType, Schema, model } from "mongoose";
 import { USER_ROLES, USER_STATUSES } from "./users.consts";
 
-const UserSchema = new Schema(
-  {
-    name: { type: String, required: true },
-    phone: { type: String, required: true, unique: true },
-    role: { type: String, enum: USER_ROLES, required: true },
-    status: { type: String, enum: USER_STATUSES, default: "ACTIVE" },
-    password: { type: String, required: true },
-  },
-  {
-    timestamps: true,
-  }
-);
+const UserSchemaDefinition = {
+  name: { type: String, required: true },
+  phone: { type: String, required: true, unique: true },
+  role: { type: String, enum: USER_ROLES, required: true },
+  status: { type: String, enum: USER_STATUSES, default: "ACTIVE" },
+  password: { type: String, required: true },
+} as const;
+
+const UserSchema = new Schema(UserSchemaDefinition, {
+  timestamps: true,
+});
 
 export const User = model("User", UserSchema);
+export type UserFilter = Partial<InferRawDocType<typeof UserSchemaDefinition>>;
