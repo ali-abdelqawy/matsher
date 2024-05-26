4fb83ecf38ce1aee67c364c00ad5fb98eacdcb6a
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-26 06:11:58 +0300

    use cookie parser and load middlewares dynamically

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import "reflect-metadata";
 import path from "path";
+import express from "express";
+import { useExpressServer } from "routing-controllers";
 import { Mongo } from "./core/utils";
-import { createExpressServer } from "routing-controllers";
-import { ErrorHandler } from "./core/middlewares";
+import cookieParser from "cookie-parser";
 
 Mongo.get().connect();
 
-const app = createExpressServer({
+let app = express();
+app.use(cookieParser());
+app = useExpressServer(app, {
   routePrefix: "/api",
   validation: {
     whitelist: true,
     forbidNonWhitelisted: true,
   },
   defaultErrorHandler: false,
-  middlewares: [ErrorHandler],
-  controllers: [path.join(__dirname + "/**/*.controller.{js,ts}")],
+  middlewares: [path.join(`${__dirname}/**/global/*.middleware.{js,ts}`)],
+  controllers: [path.join(`${__dirname}/**/*.controller.{js,ts}`)],
 });
 
 app.listen(process.env.PORT, () => console.log("matsher api is up and running!"));
