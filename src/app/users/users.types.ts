f870eba19bb6356b0ebfd97d414bd6b3ce6b52f9
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-26 06:14:41 +0300

    logged user type

--- src/app/users/users.types.ts
+++ src/app/users/users.types.ts
@@ -1,4 +1,6 @@
 import { USER_ROLES, USER_STATUSES } from "./users.consts";
+import { UserFilter } from "./users.schema";
 
 export type UserRole = (typeof USER_ROLES)[number];
 export type UserStatus = (typeof USER_STATUSES)[number];
+export type LoggedUser = Required<Pick<UserFilter, "_id" | "name" | "phone" | "role">>;
