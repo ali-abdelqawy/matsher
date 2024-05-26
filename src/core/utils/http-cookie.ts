0332573f6fa887bdd83d4483988fb20f1eacdc2f
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-26 06:08:09 +0300

    expire cookies

--- src/core/utils/http-cookie.ts
+++ src/core/utils/http-cookie.ts
@@ -1,5 +1,5 @@
 import { CookieOptions, Response } from "express";
-
+import dayjs, { ManipulateType } from "dayjs";
 export abstract class HttpCookie {
   private static enableHttps = !["LOCAL", "TESTING"].includes(process.env.NODE_ENV);
   private static options: CookieOptions = {
@@ -10,8 +10,14 @@ export abstract class HttpCookie {
     }),
   };
 
-  static set(name: string, payload: string, res: Response) {
-    res.cookie(name, payload, this.options);
+  static set(name: string, payload: string, res: Response, expiresIn: string) {
+    const [value, unit] = expiresIn.split(" ");
+    res.cookie(name, payload, {
+      ...this.options,
+      expires: dayjs()
+        .add(Number(value), unit as ManipulateType)
+        .toDate(),
+    });
   }
 
   static clear(name: string, res: Response) {
