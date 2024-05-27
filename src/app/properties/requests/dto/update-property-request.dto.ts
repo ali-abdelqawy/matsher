2198baa9a0ff2ed9bdceff6f51fe49db3474d27a
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-27 19:55:18 +0300

    fix updating property request

--- src/app/properties/requests/dto/update-property-request.dto.ts
+++ src/app/properties/requests/dto/update-property-request.dto.ts
@@ -1,11 +1,11 @@
 import { IsDecimal, IsIn, IsOptional, IsString, MaxLength } from "class-validator";
 import { PropertyPriceUnit, PropertyAreaUnit } from "../property-requests.types";
 import { PROPERTY_AREA_UNITS, PROPERTY_PRICE_UNITS } from "../property-requests.consts";
-import { DECIMAL_VALIDATION_OPTIONS } from "../../../../core/constants";
+import { DECIMAL_VALIDATION } from "../../../../core/constants";
 
 export class UpdatePropertyRequestBody {
   @IsOptional()
-  @IsDecimal(DECIMAL_VALIDATION_OPTIONS)
+  @IsDecimal(DECIMAL_VALIDATION.options)
   "area.value": string;
 
   @IsOptional()
@@ -13,7 +13,7 @@ export class UpdatePropertyRequestBody {
   "area.unit": PropertyAreaUnit;
 
   @IsOptional()
-  @IsDecimal(DECIMAL_VALIDATION_OPTIONS)
+  @IsDecimal(DECIMAL_VALIDATION.options)
   "price.value": string;
 
   @IsOptional()
