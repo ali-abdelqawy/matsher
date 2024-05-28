b71dce7d1e4b520014f8ad9b981a5a9cbdf7c9fd
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-29 00:36:02 +0300

    formatted filter type

--- src/core/globals/express.d.ts
+++ src/core/globals/express.d.ts
@@ -1,11 +1,12 @@
 import { Algorithm } from "jsonwebtoken";
-import { NodeEnv } from "../types";
+import { FormattedFilter, NodeEnv } from "../types";
 import { LoggedUser } from "../../app/users";
 
 declare global {
   namespace Express {
     interface Locals {
       user: LoggedUser;
+      filter: FormattedFilter;
     }
   }
 }
--- src/core/types.ts
+++ src/core/types.ts
@@ -3,3 +3,9 @@ import { NODE_ENVS } from "./constants";
 export type NodeEnv = (typeof NODE_ENVS)[number];
 
 export type HttpMethod = "get" | "post" | "put" | "patch" | "delete";
+
+export type FormattedFilter = {
+  limit: number;
+  skip: number;
+  project: any;
+};
