70d2043db39dcb894fb1710ae376582ea7c8b8e2
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-29 00:33:48 +0300

    rename property request service to property ad service

--- src/app/properties/ads/property-ads.service.ts
+++ src/app/properties/ads/property-ads.service.ts
@@ -1,6 +1,9 @@
+import { Types } from "mongoose";
 import { LoggedUser } from "../../users";
 import { InsertPropertyRequestBody } from "../requests/dto";
 import { PropertyAd } from "./property-ads.schema";
+import { FindRelevantRequestsQuery } from "./dto";
+import { Mongo, Paginator } from "../../../core/utils";
 
 export class PropertyAdsService {
   async insertOne(body: InsertPropertyRequestBody, user: LoggedUser) {
