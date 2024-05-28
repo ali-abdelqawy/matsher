92b7495e3dcc66fe662bdf47f299522369983466
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-29 02:21:13 +0300

    model type

--- src/core/utils/gate.ts
+++ src/core/utils/gate.ts
@@ -1,7 +1,7 @@
 import { Model, ObjectId } from "mongoose";
 
 export abstract class Gate {
-  static isCreator(entityId: string, model: typeof Model, userId: ObjectId) {
+  static isCreator(entityId: string, model: Model<any>, userId: ObjectId) {
     return model.exists({ _id: entityId, createdBy: userId });
   }
 }
