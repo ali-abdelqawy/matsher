e17051d511d49993283f38cc37ccd58b6228a0ad
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-29 00:36:41 +0300

    remove unnecessary type casting

--- src/core/decorators/logged-user.decorator.ts
+++ src/core/decorators/logged-user.decorator.ts
@@ -1,9 +1,8 @@
 import { createParamDecorator } from "routing-controllers";
-import { LoggedUser } from "../../app/users";
 
 export function User() {
   return createParamDecorator({
     required: true,
-    value: (action) => action.response.locals.user as LoggedUser,
+    value: (action) => action.response.locals.user,
   });
 }
