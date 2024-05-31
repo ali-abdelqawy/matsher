a61570e3b2776f942c2aea1843daaea737fcc14c
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-31 23:49:22 +0300

    format data before returning it to the user

--- src/core/utils/mongo.ts
+++ src/core/utils/mongo.ts
@@ -2,6 +2,9 @@ import { Model } from "mongoose";
 import { FilterDto } from "../dto";
 import { AggregateOptions } from "mongoose";
 import { Paginator } from "./paginator";
+import { Obj } from "./obj";
+import { Formatter } from "./formatter";
+import { isDate, isNumberString } from "class-validator";
 
 export class Mongo {
   public static formatFilter(options: FilterDto & { fields: string[] }) {
@@ -13,6 +16,24 @@ export class Mongo {
     };
   }
 
+  static formatData(data: any[]) {
+    return data.map((record) =>
+      Object.keys(record).reduce(
+        (formatted: any, key: string) => ({
+          ...formatted,
+          [key]: record[key],
+          ...(isDate(record[key]) && {
+            [key]: Formatter.date(record[key]),
+          }),
+          ...(isNumberString(record[key]) && {
+            [key]: Formatter.number(record[key]),
+          }),
+        }),
+        {}
+      )
+    );
+  }
+
   public static async aggregate(params: {
     model: Model<any>;
     pipelineStages: any[];
@@ -22,6 +43,9 @@ export class Mongo {
   }) {
     const { model, pipelineStages, aggOptions, limit, page } = params;
     const [{ data, meta } = { data: [], meta: {} }] = await model.aggregate(pipelineStages, aggOptions);
+    return { data: this.formatData(data), ...Paginator.getMetadata(meta.total, limit, page) };
+  }
+
   public static getModelFields(model: Model<any>, type: "pick" | "omit" = "omit", fields: string[] = []) {
     const schemaTree = (model.schema as any).tree;
     const selectedFields = Obj.omit(Obj[type](schemaTree, fields), ["__v", "id"]);
