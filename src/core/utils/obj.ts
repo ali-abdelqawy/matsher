8997774d86a6acc6bf8e0a2c84a518df3a2bc8a9
Ali Khaled <ali.abdelqawy@outlook.com>
2024-05-31 23:45:03 +0300

    omit utility

--- src/core/utils/obj.ts
+++ src/core/utils/obj.ts
@@ -2,4 +2,10 @@ export abstract class Obj {
   static pick(obj: any, fields: string[]) {
     return fields.reduce((picked: any, field) => ({ ...picked, [field]: obj[field] }), {});
   }
+
+  static omit(obj: any, fields: string[]) {
+    return Object.keys(obj)
+      .filter((key) => !fields.includes(key))
+      .reduce((filtered, key) => ({ ...filtered, [key]: obj[key] }), {});
+  }
 }
