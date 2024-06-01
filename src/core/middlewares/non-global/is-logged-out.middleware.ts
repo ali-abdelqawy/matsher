77b805446416f916fd3eca9959dc2b7a568d665a
Ali Khaled <ali.abdelqawy@outlook.com>
2024-06-01 17:08:35 +0300

    return status instead of throwing exception in is logged out middleware

--- src/core/middlewares/non-global/is-logged-out.middleware.ts
+++ src/core/middlewares/non-global/is-logged-out.middleware.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
 import { ExpressMiddlewareInterface, Middleware } from "routing-controllers";
-import { HttpException } from "../../utils";
 
 @Middleware({ type: "before", priority: 3 })
 export class IsLoggedOut implements ExpressMiddlewareInterface {
-  async use(req: Request, _res: Response, next: (err?: any) => any) {
+  async use(req: Request, res: Response, next: (err?: any) => any) {
     if (req.cookies?.token) {
-      throw new HttpException(STATUS_CODES.UNAUTHORIZED, "you must logout first");
+      res.sendStatus(STATUS_CODES.UNAUTHORIZED);
+      return;
     }
     next();
   }
