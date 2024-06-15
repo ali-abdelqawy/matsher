0cb6c9b744e150687f61bfee524f2a15168e7a25
Ali Khaled <ali.abdelqawy@outlook.com>
2024-06-15 03:56:16 +0300

    fix object utilities return type

--- src/core/utils/obj.ts
+++ src/core/utils/obj.ts
@@ -1,11 +1,11 @@
 export abstract class Obj {
   static pick(obj: any, fields: string[]) {
-    return fields.reduce((picked: any, field) => ({ ...picked, [field]: obj[field] }), {});
+    return fields.reduce((picked: any, field) => ({ ...picked, [field]: obj[field] }), {} as any);
   }
 
   static omit(obj: any, fields: string[]) {
     return Object.keys(obj)
       .filter((key) => !fields.includes(key))
-      .reduce((filtered, key) => ({ ...filtered, [key]: obj[key] }), {});
+      .reduce((filtered, key) => ({ ...filtered, [key]: obj[key] }), {} as any);
   }
 }
