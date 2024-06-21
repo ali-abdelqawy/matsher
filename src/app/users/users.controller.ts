d1253e10c993b9767d3e1cd290d20374c2a1eb2f
Ali Khaled <ali.abdelqawy@outlook.com>
2024-06-21 16:21:26 +0300

    detailed swagger documentation for user stats endpoint

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Get, JsonController, QueryParams, UseBefore } from "routing-controllers
 import { UsersService } from "./users.service";
 import { FindUserStatsQuery } from "./dto";
 import { Authorize } from "../../core/middlewares";
+import { OpenAPI } from "routing-controllers-openapi";
 
 @JsonController("/users", { transformResponse: false })
 export class UsersController {
@@ -12,6 +13,25 @@ export class UsersController {
   }
 
   @Get("/stats")
+  @OpenAPI({
+    summary: "User Statistics",
+    description:
+      "This endpoint allows admins to know about how many ads/requests exist for a client/agent and the total amount/count of them",
+    responses: {
+      "400": {
+        description: "Invalid input",
+      },
+      "401": {
+        description: "User is not authenticated",
+      },
+      "403": {
+        description: "The authenticated user is not an admin",
+      },
+      "200": {
+        description: "Successful response",
+      },
+    },
+  })
   @UseBefore(Authorize(["ADMIN"]))
   async findStats(@QueryParams() query: FindUserStatsQuery) {
     return this.service.findStats(query);
