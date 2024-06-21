d49654df53c0c0532b8082d9027676dbcec652cc
Ali Khaled <ali.abdelqawy@outlook.com>
2024-06-21 03:07:32 +0300

    fix middlewares

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import "reflect-metadata";
 import "./core/globals";
 import { join } from "path";
 import express from "express";
-import { createExpressServer } from "routing-controllers";
+import { useExpressServer } from "routing-controllers";
 import cookieParser from "cookie-parser";
 import { PUBLIC_FOLDER_PATH } from "./core/constants";
 import { Db } from "./core/db";
 import { Swagger } from "./core/swagger";
 
-const app = createExpressServer({
+let app = express();
+app.use(cookieParser(), express.static(PUBLIC_FOLDER_PATH));
+app = useExpressServer(app, {
   routePrefix: "/api",
   validation: {
     whitelist: true,
@@ -18,7 +20,6 @@ const app = createExpressServer({
   middlewares: [join(`${__dirname}/**/global/*.middleware.{js,ts}`)],
   controllers: [join(`${__dirname}/**/*.controller.{js,ts}`)],
 });
-app.use(cookieParser(), express.static(PUBLIC_FOLDER_PATH));
 
 Swagger.generate();
 Db.get().connect();
