ffcbf4d1c72e3f10febc9b3aaf7b5d5cee2e44b9
Ali Khaled <ali.abdelqawy@outlook.com>
2024-06-21 03:06:34 +0300

    mark is creator function as async

--- src/core/utils/gate.ts
+++ src/core/utils/gate.ts
@@ -1,7 +1,7 @@
 import { Model, ObjectId } from "mongoose";
 
 export abstract class Gate {
-  static isCreator(entityId: string, model: Model<any>, userId: ObjectId) {
+  static async isCreator(entityId: string, model: Model<any>, userId: ObjectId) {
     return model.exists({ _id: entityId, createdById: userId });
   }
 }
